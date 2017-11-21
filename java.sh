5eda67ae6042ad8a55c76657e2e334ae0dcf0b5f
Ilya Dmitrichenko <errordeveloper@gmail.com>
2017-11-21 10:56:39 +0000

    Add `-Djava.security.egd=file:/dev/urandom` as all our apps ask for it

--- java.sh
+++ java.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ -z "$JAVA_OPTS" ]; then
-  JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -XX:UseG1GC"
+  JAVA_OPTS="-XX:+UnlockExperimentalVMOptions -XX:+UseCGroupMemoryLimitForHeap -XX:MaxRAMFraction=1 -XX:UseG1GC -Djava.security.egd=file:/dev/urandom"
 fi
 
 java $JAVA_OPTS $*
